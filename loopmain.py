7985ee7bdf31fad2efed218165cafa283c2fc1f9
mayur2402 <mayurdimble99@gmail.com>
2024-04-21 08:21:40 +0530

    20-04-2024

---
+++ loopmain.py
@@ -0,0 +1,16 @@
+
+from loopmodule import Loop
+from loopmodule import WhileLoop
+
+
+def main():
+    print("Enter number")
+    no = int(input())
+    
+    Loop(no)
+    WhileLoop(no)
+
+if(__name__ == "__main__"):
+    main()
+
+
