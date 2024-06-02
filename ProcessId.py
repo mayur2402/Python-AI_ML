994e7942df2fee96424f23d62aac5863f6dee9fb
mayur2402 <mayurdimble99@gmail.com>
2024-06-02 14:52:03 +0530

    directory cleaner

---
+++ ProcessId.py
@@ -0,0 +1,50 @@
+import psutil
+import smtplib
+
+def DisplayProcess():
+    print("List of running processess are : ")
+
+    print("-----------------------------------")
+
+    for proc in psutil.process_iter(['pid','name','username']):
+        print(proc.info)
+
+    print("-----------------------------------------")
+
+
+def CreateLog(FileName = "Log.log"):
+    fd = open(FileName,"w")
+
+    separator = "-"*70
+
+    fd.write(separator + "\n")
+    fd.write("Mayur Process Log" + "\n")
+    fd.write(separator + "\n")
+    fd.write("Content of log file")
+    datadict = {}
+    icnt = 0
+    for proc in psutil.process_iter(['pid','name','username']):
+        datadict[icnt] = proc.info
+        icnt = icnt + 1
+    jcnt = 0
+    for data in datadict:
+        fd.write(str(datadict[data]) + "\n")
+        jcnt = jcnt + 1
+
+    fd.write(separator + "\n")
+
+    fd.close()
+
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login("mayurdimble99@gmail.com", "Sarika@1234567890")
+    message = "Message_you_need_to_send"
+    s.sendmail("mayurdimble99@gmail.com", "mayurdimble412105@gmail.com", message)
+    s.quit()
+
+def main():
+    #DisplayProcess()
+    CreateLog()
+
+if __name__ == "__main__":
+    main()
---
+++ ProcessIdSchedule.py
@@ -0,0 +1,51 @@
+import os
+import time
+import psutil
+import smtplib
+import schedule
+
+def DisplayProcess():
+    print("List of running processess are : ")
+
+    print("-----------------------------------")
+
+    for proc in psutil.process_iter(['pid','name','username']):
+        print(proc.info)
+
+    print("-----------------------------------------")
+
+
+def CreateLog(FileName = "Log.log"):
+
+    fd = open(FileName,"a")
+
+    separator = "-"*70
+
+    fd.write(separator + "\n")
+    fd.write("Mayur Process Log" + "\n")
+    fd.write(separator + "\n")
+    fd.write("Content of log file")
+    datadict = {}
+    icnt = 0
+    for proc in psutil.process_iter(['pid','name','username']):
+        datadict[icnt] = proc.info
+        icnt = icnt + 1
+    jcnt = 0
+    for data in datadict:
+        fd.write(str(datadict[data]) + "\n")
+        jcnt = jcnt + 1
+
+    fd.write(separator + "\n")
+
+    fd.close()
+
+def main():
+    #DisplayProcess()
+    schedule.every(1).minutes.do(CreateLog)    
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+if __name__ == "__main__":
+    main()
